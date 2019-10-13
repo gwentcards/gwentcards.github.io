a799ed517f8320b503887a23033001ebd7badc2a
vzx <vzx@users.noreply.github.com>
2019-10-13 11:05:46 +0200

    Updates

---
+++ precache-manifest.be6492bcaab876e7af541f3876c713f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e81900c52006743792ee6ced6541e052",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4a5b687aee39795dfa6",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "29ba23bdc224327c4dcd",
+    "url": "/static/css/main.d9ecc808.chunk.css"
+  },
+  {
+    "revision": "d4a5b687aee39795dfa6",
+    "url": "/static/js/2.c3ee76fd.chunk.js"
+  },
+  {
+    "revision": "29ba23bdc224327c4dcd",
+    "url": "/static/js/main.5e57b7ef.chunk.js"
+  },
+  {
+    "revision": "37d36d1a26c32da0c75d",
+    "url": "/static/js/runtime-main.16038842.js"
+  }
+]);
